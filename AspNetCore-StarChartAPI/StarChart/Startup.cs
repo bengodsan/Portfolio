3932f5d536a2f88c2af3ffaea27dcf813e1ffdfe
BennieGamali <bengodsan@hotmail.com>
2019-07-22 15:47:03 +0200

    Committing Starchart.Api

---
+++ AspNetCore-StarChartAPI/StarChart/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using StarChart.Data;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace StarChart
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("Starchart"));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "StarChart.Api", Version = "v1.0.0" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "StarChart.Api");
+            });
+
+            app.UseMvc();
+        }
+    }
+}
